396690e286dedb1bcae0dd291924cfe5bc6063b4
abhynadar <vabhyandar@yahoo.com>
2020-10-12 13:59:48 +0530

    removed unused refs '

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { URL } from "../constants/Urls";
 import { Home } from "../controls/home/Home";
 
 const HomePage = () => {
