b5ced2d0d2903f66d431fa6c1525710a5f1adae8
abhynadar <vabhyandar@yahoo.com>
2020-10-18 19:38:26 +0530

    fixed error on home

--- src/controls/home/Home.tsx
+++ src/controls/home/Home.tsx
@@ -3,13 +3,11 @@ import React from "react";
 import "./Home.css";
 import { Header } from "../header/Header";
 import { Posts } from "../posts/Posts";
-import { Thoughts } from "../thoughts/Thoughts";
 
 const Home = () => {
   return (
     <div >
       <Header />
-      {/* <Thoughts /> */}
       <Posts />
     </div>
   );
